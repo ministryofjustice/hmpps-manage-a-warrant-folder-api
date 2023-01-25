6eb7a15f7a8a34155a722e16158c7b5e9413b245
Luke Harper <luke.harper@digital.justice.gov.uk>
2023-01-25 13:56:34 +0000

    Fixing exception class.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageawarrantfolderapi/relevantremand/UnsupportedCalculationException.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsmanageawarrantfolderapi/relevantremand/UnsupportedCalculationException.kt
@@ -2,4 +2,4 @@ package uk.gov.justice.digital.hmpps.hmppsmanageawarrantfolderapi.relevantremand
 
 class UnsupportedCalculationException(
   override val message: String
-) : Throwable(message)
+) : Exception(message)
